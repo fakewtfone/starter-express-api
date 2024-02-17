2136a3f5f1bcf67f8a6e3a5fe77e6dab13b2ebbd
fakewtfone <160347462+fakewtfone@users.noreply.github.com>
2024-02-18 01:40:44 +0530

    adding cross-site
    adding cross-site

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.all('/', (req, res) => {
     res.send('Server is running, thanks again!')
 })
